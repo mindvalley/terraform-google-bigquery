f15bb3ba1a0d941941d00c5254de529c2e04c533
Alex Co <tuanclq@gmail.com>
2021-11-18 12:19:53 +0700

    remove unused output
    Signed-off-by: Alex Co <tuanclq@gmail.com>

--- modules/access/outputs.tf
+++ modules/access/outputs.tf
@@ -14,11 +14,6 @@
  * limitations under the License.
  */
 
-output "authorized_views" {
-  value       = google_bigquery_dataset_access.authorized_view
-  description = "Authorized views for the dataset"
-}
-
 output "authorized_roles" {
   value       = google_bigquery_dataset_access.access_role
   description = "Authorized roles for the dataset"
